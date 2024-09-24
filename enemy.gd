09d0a5739bf3d2040c65b9b7fed2082149e6d967
QNguyen25 <quangng504@gmail.com>
2024-09-24 13:38:00 -0500

    timer player and enemy

--- enemy.gd
+++ enemy.gd
@@ -1,11 +1,17 @@
 extends RigidBody2D
 
+@export var enemy_scene: PackedScene
+var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var enemy_types : Array = $AnimatedSprite2D.sprite_frames.get_animation_name()
+	$AnimatedSprite2D.play(enemy_types[randi() % enemy_types.size()])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	pass 
+	
+	
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
---
+++ main.gd
@@ -0,0 +1,50 @@
+extends Node
+
+@export var enemy_scene: PackedScene
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func game_over():
+	$EnemyTimer.stop()
+	$ScoreTimer.stop()
+	
+func new_game():
+	score = 0
+	$Player.start($PlayerSpawn.position)
+	$StartTimer.start()
+	
+	
+	
+func _on_score_timer_timeout():
+	score += 1
+	
+	
+func _on_start_timer_timeout():
+	$EnemyTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_enemy_timer_timeout():
+	var enemy = enemy_scene.instantiate()
+	
+	var enemy_spawn_location = $EnemyPath/EnemySpawnLocation
+	enemy_spawn_location.progress_ratio = randf()
+	
+	var direction = enemy_spawn_location.rotation + PI/2
+	
+	enemy.position = enemy_spawn_location.position
+	direction += randf_range(-PI/4, PI/4)
+	enemy.rotation = direction
+	
+	
+	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	enemy.linear_velocity = velocity.rotated(direction)
